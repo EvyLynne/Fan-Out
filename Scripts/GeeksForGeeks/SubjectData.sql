6bd7dd3b7c53ef49cd097b8ec54957ebc1d63347
Evelyn <evelynlivant@gmail.com>
2025-12-27 17:14:57 -0500

    Update and rename SubjectData.txt to Scripts/GeeksForGeeks/SubjectData.sql

---
+++ Scripts/GeeksForGeeks/SubjectData.sql
@@ -0,0 +1,15 @@
+USE [Denormalization]
+GO
+
+INSERT INTO [dbo].[Subject_Geeks_For_Geeks]
+           ([ClassID]
+           ,[Subject])
+     VALUES
+          ('C101','Algebra'),
+	    ('C101','Calculus'),
+		('C102','Chemistry'),
+		('C101','Geometry'),
+		('C102','Physics'),
+		('C101','Trigonometry')
+
+GO
