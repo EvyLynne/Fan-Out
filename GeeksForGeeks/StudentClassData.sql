9a5f4db472327b41cc438a4bf9a0f79760093196
Evelyn <evelynlivant@gmail.com>
2025-12-27 17:03:01 -0500

    Update and rename StudentClassData.txt to GeeksForGeeks/StudentClassData.sql
    Create insert script and move to Geeks for Geeks folder.

---
+++ GeeksForGeeks/StudentClassData.sql
@@ -0,0 +1,10 @@
+USE [Denormalization]
+GO
+
+INSERT INTO [dbo].[StudentClass_Geeks_For_Geeks]
+           ([StudentID]
+           ,[ClassID])
+     VALUES
+           (1,'C101'),
+		   (2,'C102')
+GO
